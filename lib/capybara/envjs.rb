759b8f6cf017af4dfc8d3011f893d79691581816
Steven Parkes <smparkes@smparkes.net>
2010-05-19 10:33:25 -0700

    don't assume cucumber if no world ... some people had trouble

--- lib/capybara/envjs.rb
+++ lib/capybara/envjs.rb
@@ -1,6 +1,6 @@
 require 'capybara'
 require 'capybara/driver/envjs_driver'
 
-if Object.const_defined? :Cucumber
+if Object.const_defined? :Cucumber and Object.respond_to? :World
   require 'capybara/envjs/cucumber'
 end
